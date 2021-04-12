41a122b56fd69e98b28ef24d7a229f9c71cd58aa
Clément <50326440+d3dpanda@users.noreply.github.com>
2021-04-12 09:57:29 +0400

    Update bdd.php

--- login/bdd.php
+++ login/bdd.php
@@ -1,5 +1,5 @@
 <?php
-    $bdd = new PDO('mysql:host=localhost;dbname=utilisateur', 'd3dpanda', 'Programmingislife_18072003');
+    $bdd = new PDO('mysql:host=localhost;dbname=utilisateur', 'd3dpanda', 'Programmingislife__18juillet2003');
     $test = "CREATE DATABASE `Test`";
     $sql = "CREATE TABLE User (`id` INT PRIMARY KEY AUTO_INCREMENT, `pseudo` VARCHAR(60), `email` VARCHAR(60), `mdp` VARCHAR(60));  INSERT INTO User (`pseudo`, `email`, `mdp`) VALUES ('test1', 'test1@gmail.com', '1234')";
     $bdd->exec($sql);
