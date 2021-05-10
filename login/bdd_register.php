98b079beaf93042174cc270a98ad0187428f4d56
d3dpanda <cleml97430@gmail.com>
2021-05-10 17:08:36 +0400

    modifié :         login/bdd_register.php

--- login/bdd_register.php
+++ login/bdd_register.php
@@ -7,24 +7,22 @@
     include 'register.php';
  // Enregistrement des informations dans la table lors de la première connexcion
 
-   $pseudo = $_POST['pseudo'];
-   $email =  $_POST['email'];
-   $mdp = $_POST['password'];
+//   $pseudo = $_POST['pseudo'];
+// $email =  $_POST['email'];
+//   $mdp = $_POST['password'];
     
 // Vérification des doublons dans la base de données
 // A COMPLÉTER !!!!!!
-    try{
-//        $sql = "INSERT INTO User (pseudo, email, mdp) VALUES ('aa', 'aa', 'aa') ";
-        $sql = "INSERT INTO User (pseudo, email, mdp) VALUES ('".$pseudo."', '".$email."', '".$mdp."') ";
-        $count =  $bdd->exec($sql); 
-        
-    }
-    catch (Exception $e) {
-        echo "foiré"; 
-    };
-    echo "test";
+    $sql = "INSERT INTO User (pseudo, email, mdp) VALUES ('aa', 'aa', 'aa') ";
+//    $sql = "INSERT INTO User (pseudo, email, mdp) VALUES ('".$pseudo."', '".$email."', '".$mdp."') ";
+    $verification = "SELECT COUNT(*) FROM User WHERE pseudo='aa';";
+    $count =  $bdd->exec($sql); 
+    $verif = $bdd->exec($verification);
+    $showTables = $bdd->exec($tables);
+    echo $verif;
+    
     sleep(2);
-    header("Location: ../Sokoban/Sokoban.html"); 
+   // header("Location: ../Sokoban/Sokoban.html"); 
     
 ?>
 
