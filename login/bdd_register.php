d89b9e96332d875750ad569bd5cbac50126f0712
d3dpanda <cleml97430@gmail.com>
2021-05-10 11:56:49 +0400

    modifié :         login/bdd_register.php

--- login/bdd_register.php
+++ login/bdd_register.php
@@ -13,20 +13,11 @@
     
 // Vérification des doublons dans la base de données
 // A COMPLÉTER !!!!!!
-    $results = $bdd->query("SELECT EXISTS (SELECT mdp FROM User WHERE pseudo='".$pseudo.") AS test;");
-    while ($content = $results->fetch()) {
-        $row = $content['pseudo'];
-        $sql = "INSERT INTO User(pseudo) VALUES ('".$results."');";
-//        if ($row == TRUE) {
-//            header("Location: ./register.php");
-//        }
-//        else {
-//            $sql = "INSERT INTO User (pseudo, email, mdp) VALUES ('".$pseudo."', '".$email."', '".$mdp."') ";
-//            $count =  $bdd->exec($sql); 
-//            sleep(2);
-//            header("Location: ../Sokoban/Sokoban.html");    
-//        };
-    }
+    
+    $sql = "INSERT INTO User (pseudo, email, mdp) VALUES ('".$pseudo."', '".$email."', '".$mdp."') ";
+    $count =  $bdd->exec($sql); 
+    sleep(2);
+    header("Location: ../Sokoban/Sokoban.html");    
 
     
     
