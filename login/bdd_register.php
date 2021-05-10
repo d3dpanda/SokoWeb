0299953daa7c3e6969a4abaa4eaa4645a5c238a4
d3dpanda <cleml97430@gmail.com>
2021-05-10 18:47:19 +0400

    modifié :         login/bdd_register.php

--- login/bdd_register.php
+++ login/bdd_register.php
@@ -14,24 +14,24 @@
     
 // Vérification des doublons dans la base de données
 
-    $sql = "INSERT INTO User (pseudo, email, mdp) VALUES ('clement', 'clement@gmail.com', 'clm') ";
-//    $sql = "INSERT INTO User (pseudo, email, mdp) VALUES ('".$pseudo."', '".$email."', '".$mdp."') ";
+//    $sql = "INSERT INTO User (pseudo, email, mdp) VALUES ('clement', 'clement@gmail.com', 'clm') ";
+    $sql = "INSERT INTO User (pseudo, email, mdp) VALUES ('".$pseudo."', '".$email."', '".$mdp."') ";
 
 //  Commande SQL vérifiant la présence du pseudo dans la base de donnée.
 // Si la commande renvoie 1, alors le pseudo existe déjà
 // Si la commande renvoie 0, alors le pseudo n'existe pas
-    $verification = $bdd->query("SELECT COUNT(pseudo) FROM User WHERE pseudo='clement'");
+    $verification = $bdd->query("SELECT COUNT(pseudo) FROM User WHERE pseudo='".$pseudo."'");
    
     $count =  $bdd->exec($sql); 
 // essai de la sélection de la colonne pseudo, avec le pseudo renseigné par l'utilisateur
     try{
         while($data = $verification->fetch()) {
-            echo $data['pseudo'];
-            if ($data['pseudo'] == 1) { // Si le pseudo existe déjà
+            echo $data[0]."\n";
+            if ($data[0] == 1) { // Si le pseudo existe déjà
                 echo "Compte existant\n";
-        
+                header("Location: register.php");
             }
-            elseif ($data['pseudo'] == 0) { // Si le pseudo n'existe pas 
+            elseif ($data[0] == 0) { // Si le pseudo n'existe pas 
                 echo "Nouveau compte crée\n"; 
                 sleep(1);
                 header("Location: ../Sokoban/Sokoban.html");
