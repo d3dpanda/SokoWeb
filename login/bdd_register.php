eef9f793673423bb54e8e17bcd908af16b9d79c5
d3dpanda <cleml97430@gmail.com>
2021-05-10 18:28:35 +0400

    modifié :         login/bdd_register.php

--- login/bdd_register.php
+++ login/bdd_register.php
@@ -5,24 +5,39 @@
 
     include 'bdd.php';
     include 'register.php';
+
  // Enregistrement des informations dans la table lors de la première connexcion
 
-//   $pseudo = $_POST['pseudo'];
-// $email =  $_POST['email'];
-//   $mdp = $_POST['password'];
+   $pseudo = $_POST['pseudo'];
+   $email =  $_POST['email'];
+   $mdp = $_POST['password'];
     
 // Vérification des doublons dans la base de données
 // A COMPLÉTER !!!!!!
-    $sql = "INSERT INTO User (pseudo, email, mdp) VALUES ('aa', 'aa', 'aa') ";
+    $sql = "INSERT INTO User (pseudo, email, mdp) VALUES ('clement', 'clement@gmail.com', 'clm') ";
 //    $sql = "INSERT INTO User (pseudo, email, mdp) VALUES ('".$pseudo."', '".$email."', '".$mdp."') ";
-    $verification = "SELECT COUNT(*) FROM User WHERE pseudo='aa';";
+   
+    $verification = $bdd->query("SELECT COUNT(pseudo) FROM User WHERE pseudo='".$pseudo."'");
+   
     $count =  $bdd->exec($sql); 
-    $verif = $bdd->exec($verification);
-    $showTables = $bdd->exec($tables);
-    echo $verif;
     
-    sleep(2);
-   // header("Location: ../Sokoban/Sokoban.html"); 
+    try{
+        while($data = $verification->fetch()) {
+            echo $data['pseudo'];
+            if ($data['pseudo'] == 1) {
+                echo "Compte existant\n";
+        
+            }
+            elseif ($data['pseudo'] == 0) {
+                echo "Nouveau compte crée\n";
+                sleep(1);
+                header("Location: ../Sokoban/Sokoban.html");
+            }
+        };
+    }
+    catch (Exception $e) {
+        die('Erreur: '.$e->getMessage());
+    }
     
 ?>
 
